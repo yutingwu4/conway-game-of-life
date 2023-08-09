ffae35fcebad7be96df9f6af6864e37b8bd7d531
Julie W <scorp.only@gmail.com>
2023-08-08 21:12:02 -0700

    create store for Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import useBoard from './hooks/useBoard';
 
 function App() {
+const {board, toggleCell} = useBoard(5, 5, false); 
 
-  return (
-    <>
-    </>
+// create play vs. pause button
+  // state variable: is the game executing?
+
+return (
+    <table>
+      {board.map((row, y) => <tr>{row.map((cell, x) => <td onClick={() => toggleCell(x, y)}>{cell ? 1 : 0}</td>)}</tr>)}
+    </table>
   )
 }
 
-export default App
+export default App;
---
+++ src/hooks/useBoard.ts
@@ -0,0 +1,27 @@
+import { useState, useCallback } from "react";
+
+const generateDefaultBoard = (r: number, c: number, val: boolean) => {
+  return new Array(r).fill(0).map(() => new Array(c).fill(val));
+};
+
+// track current state of board
+/* responsible for:
+1. creating a new grid with a set number of rows and cols
+2. assigning initial cell value on creation
+3. retrieving and modifying value of a cell, given r and c
+*/
+const useBoard = (r: number, c: number, initialVal: boolean) => {
+  const [board, setBoard] = useState(generateDefaultBoard(r, c, initialVal));
+
+  const toggleCell = useCallback((targetX: number, targetY: number) => {
+    setBoard((prevBoard) => {
+      return prevBoard.map((row, y) =>
+        row.map((cell, x) => (x == targetX && y == targetY ? !cell : cell))
+      );
+    });
+  }, []);
+
+  return { board, toggleCell };
+};
+
+export default useBoard;
